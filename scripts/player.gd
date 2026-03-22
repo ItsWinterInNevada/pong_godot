5d3cc22ba7454ed46d11390ae94e00f146cf1558
ItsWinterInNevada <itswinterinnevada@mailbox.org>
2026-03-22 16:42:19 +0000

    Add script for separate player and computer movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,8 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_up", "move_down")
 	if direction:
 		velocity.y = direction * SPEED
